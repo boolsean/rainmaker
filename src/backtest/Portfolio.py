532de083f6ee765abc344f1564e7496a39ce6438
gdm <seankim@cpe-24-90-240-88.nyc.res.rr.com>
2016-04-27 22:21:59 -0400

    old changes to py Portfolio (i dunno)

--- src/backtest/Portfolio.py
+++ src/backtest/Portfolio.py
@@ -12,10 +12,25 @@ class Portfolio(object):
 
         self._orders_by_name[name].append((date, side, qty, px))
 
+    def calc_pnl(self, name):
+        orders = self._orders_by_name[name]
+        agg_pnl = 0.0
+        for o in orders:
+            side = -1
+            if o[1][0] == 'S':
+                side = 1
+            cur_pnl = o[3]*o[2]*side
+            print "calced pnl:%f from side=%d qty=%d px=%f" % (cur_pnl, side, o[2], o[3])
+
+            agg_pnl += cur_pnl
+        return agg_pnl
 
     def print_orders(self):
 
         for k, v in self._orders_by_name.iteritems():
-            print k, v
+            print k
+            print self.calc_pnl(k)
+            for o in v:
+                print o
         pass
 
